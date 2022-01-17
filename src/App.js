c2eb560c6e881b9795ed2a0e1df94e9e3b2e3876
PettLe <petewaterz@gmail.com>
2022-01-17 11:06:19 +0200

    simple API fetch to get one movie and poster

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [imgUrl, setImgUrl] = useState("");
+
+  useEffect(() => {
+    const getDatabase = async () => {
+      let data = await fetch("http://www.omdbapi.com/?apikey=f2822ef&t=Scream");
+      let movies = await data.json().then((movies) => {
+        console.log(movies);
+        console.log(movies.Poster);
+        setImgUrl(movies.Poster);
+      });
+    };
+    getDatabase();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Pette Movie Database</h1>
+      <img src={imgUrl} alt="Scream Poster" />
+    </div>
+  );
 }
 
 export default App;
